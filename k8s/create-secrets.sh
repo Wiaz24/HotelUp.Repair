1b25a8ede2da84b2be8d7c1f8fb2f8685143033a
Hubert Damaszko <73695158+Wiaz24@users.noreply.github.com>
2025-01-30 14:48:28 +0100

    env variables can be set in k8s deployment (#4)

---
+++ k8s/create-secrets.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+NAMESPACE=$1
+if [ -z "$1" ]; then
+    NAMESPACE="hotelup"
+fi
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+cat << EOF | kubectl apply -f -
+    apiVersion: v1
+    kind: Secret
+    metadata:
+        name: repair-secret
+        namespace: ${NAMESPACE}
+    type: Opaque
+    data:
+        OAUTH2_CLIENT_ID: $(echo -n "`aws ssm get-parameter --name /HotelUp.Repair/Production/Oidc/ClientId --with-decrypt --output text --profile wiaz --region us-east-1 --query Parameter.Value`" | base64 -w0)
+        OAUTH2_CLIENT_SECRET: $(echo -n "`aws ssm get-parameter --name /HotelUp.Repair/Production/Oidc/ClientSecret --with-decrypt --output text --profile wiaz --region us-east-1 --query Parameter.Value`" | base64 -w0)
+        OAUTH2_METADATA_URL: $(echo -n "`aws ssm get-parameter --name /HotelUp.Repair/Production/Oidc/MetadataAddress --with-decrypt --output text --profile wiaz --region us-east-1 --query Parameter.Value`" | base64 -w0)
+        POSTGRES_DB: $(echo -n "`aws ssm get-parameter --name /HotelUp.Repair/Production/Postgres/ConnectionString --with-decrypt --output text --profile wiaz --region us-east-1 --query Parameter.Value`" | base64 -w0)
+EOF
